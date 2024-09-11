cbd37e6d76e6da1967e43ba381e414c03544b0ab
Ronald Cardozo <ronaldcoyo@gmail.com>
2024-09-10 23:27:51 -0500

    Correcion de BD y models

--- gestion_bd/administracion/models.py
+++ gestion_bd/administracion/models.py
@@ -43,17 +43,14 @@ class Intervinientes(models.Model):
 
 # Modelo Productos
 class Productos(models.Model):
-    id = models.AutoField(primary_key=True)  # ID auto-incremental
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     clase = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True)
     cantidad = models.IntegerField()
 
     class Meta:
-        db_table = 'Productos'  # Nombre de la tabla en la base de datos
-
-    def __str__(self):
-        return self.nombre
+        db_table = 'Productos'
 
 # Modelo Cliente
 class Cliente(models.Model):
